6dc10f8152e5d8904671aaba427c886601b2ad09
Phoenix <47275834+xCelestialPhoenix@users.noreply.github.com>
2020-03-11 18:44:30 +0800

    Revert to e7d3dbe

--- src/main/java/seedu/address/commons/core/GuiSettings.java
+++ src/main/java/seedu/address/commons/core/GuiSettings.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 
 /**
  * A Serializable class that contains the GUI settings.
- * Guarantees: immutable.
+ * Guaranteed: immutable.
  */
 public class GuiSettings implements Serializable {
 
