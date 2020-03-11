571f106503d5dc00695e4675b6b086117a4de151
Loh Sze Ying <bloom9334@hotmail.com>
2020-03-11 17:35:17 +0800

    Refactor Address Book feature's command name to include "Ab" in front

---
+++ src/main/java/seedu/address/logic/parser/AbDeleteCommandParser.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.AbDeleteCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new AbDeleteCommand object
+ */
+public class AbDeleteCommandParser implements Parser<AbDeleteCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AbDeleteCommand
+     * and returns a AbDeleteCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AbDeleteCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new AbDeleteCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, AbDeleteCommand.MESSAGE_USAGE), pe);
+        }
+    }
+
+}
--- src/test/java/seedu/address/logic/commands/AbRemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AbRemarkCommandTest.java
@@ -14,7 +14,7 @@ import seedu.address.model.person.Person;
 import seedu.address.model.person.Remark;
 import seedu.address.testutil.PersonBuilder;
 
-class RemarkCommandTest {
+class AbRemarkCommandTest {
     private static final String REMARK_STUB = "Some remark";
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
@@ -24,14 +24,14 @@ class RemarkCommandTest {
         Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
         Person editedPerson = new PersonBuilder(firstPerson).withRemark(REMARK_STUB).build();
 
-        RemarkCommand remarkCommand = new RemarkCommand(INDEX_FIRST_PERSON, new Remark(editedPerson.getRemark().value));
+        AbRemarkCommand abRemarkCommand = new AbRemarkCommand(INDEX_FIRST_PERSON, new Remark(editedPerson.getRemark().value));
 
-        String expectedMessage = String.format(RemarkCommand.MESSAGE_ADD_REMARK_SUCCESS, editedPerson);
+        String expectedMessage = String.format(AbRemarkCommand.MESSAGE_ADD_REMARK_SUCCESS, editedPerson);
 
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
         expectedModel.setPerson(firstPerson, editedPerson);
 
-        assertCommandSuccess(remarkCommand, model, expectedMessage, expectedModel);
+        assertCommandSuccess(abRemarkCommand, model, expectedMessage, expectedModel);
     }
 
 }
