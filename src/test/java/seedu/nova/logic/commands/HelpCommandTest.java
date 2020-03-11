6715b61bb21012411e9955d0f19c7632daedfde8
wahken_win_ubuntu <t.1999ken@gmail.com>
2020-03-11 17:14:10 +0800

    changed name to nova

---
+++ src/test/java/seedu/nova/logic/commands/HelpCommandTest.java
@@ -0,0 +1,20 @@
+package seedu.nova.logic.commands;
+
+import static seedu.nova.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.nova.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.nova.model.Model;
+import seedu.nova.model.ModelManager;
+
+public class HelpCommandTest {
+    private Model model = new ModelManager();
+    private Model expectedModel = new ModelManager();
+
+    @Test
+    public void execute_help_success() {
+        CommandResult expectedCommandResult = new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        assertCommandSuccess(new HelpCommand(), model, expectedCommandResult, expectedModel);
+    }
+}
