626b80207bb87c9f2419d1298053f006b54644ca
BryanYap972 <bryanyap972@gmail.com>
2020-03-18 03:19:00 +0800

    Changed package and directory from seedu.address to seedu.NOVA

---
+++ src/main/java/seedu/NOVA/logic/parser/LogicParser.java
@@ -0,0 +1,83 @@
+package seedu.NOVA.logic.parser;
+
+import static seedu.NOVA.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import seedu.NOVA.logic.commands.AddressBookCommands.AbHelpCommand;
+import seedu.NOVA.logic.commands.Command;
+import seedu.NOVA.logic.commands.Common.ExitCommand;
+import seedu.NOVA.logic.commands.Common.NavCommand;
+import seedu.NOVA.logic.parser.exceptions.ParseException;
+import seedu.NOVA.model.Model;
+
+/**
+ * Class for logic parser
+ */
+public class LogicParser {
+    /**
+     * Used for initial separation of command word and args.
+     */
+    private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
+
+    private final NavCommandParser navCommandParser;
+    private final AddressBookParser addressBookParser;
+    private final EventParser eventParser;
+    private final ScheduleParser scheduleParser;
+    private final ProgresstrackerParser progresstrackerParser;
+    private Model model;
+
+    public LogicParser(Model model) {
+        //Create parser objects for each page/ feature
+        navCommandParser = new NavCommandParser();
+        addressBookParser = new AddressBookParser();
+        eventParser = new EventParser();
+        scheduleParser = new ScheduleParser();
+        progresstrackerParser = new ProgresstrackerParser();
+        this.model = model;
+    }
+
+    /**
+     * Parses user input into command for execution.
+     * @param userInput full user input string
+     * @return the command based on the user input
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public Command parseCommand(String userInput) throws ParseException {
+        final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
+        if (!matcher.matches()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AbHelpCommand.MESSAGE_USAGE));
+        }
+
+        final String commandWord = matcher.group("commandWord");
+        final String arguments = matcher.group("arguments").trim();
+
+        if (commandWord.equals(NavCommand.COMMAND_WORD)) {
+            return navCommandParser.parse(arguments);
+        } else if (commandWord.equals(ExitCommand.COMMAND_WORD)) {
+            return new ExitCommand();
+        } else {
+            ModeEnum mode = model.getMode().getModeEnum();
+
+            //check mode
+            switch (mode) {
+
+            case ADDRESSBOOK:
+                return addressBookParser.parseCommand(userInput);
+
+            case EVENT:
+                return eventParser.parseCommand(userInput);
+
+            case SCHEDULER:
+                return scheduleParser.parseCommand(userInput);
+
+            case PROGRESSTRACKER:
+                return progresstrackerParser.parseCommand(userInput);
+
+            default:
+                throw new ParseException("No such mode");
+            }
+        }
+    }
+}
