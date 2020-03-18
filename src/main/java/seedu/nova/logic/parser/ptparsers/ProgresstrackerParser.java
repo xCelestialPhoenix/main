2b28dcb6958081a77e7d266bc031e01035afc4fe
BryanYap972 <bryanyap972@gmail.com>
2020-03-18 15:54:44 +0800

    Changed seedu.NOVA to seedu.nova. Changed command format error handling.

---
+++ src/main/java/seedu/nova/logic/parser/ptparsers/ProgresstrackerParser.java
@@ -0,0 +1,17 @@
+package seedu.nova.logic.parser.ptparsers;
+
+import seedu.nova.logic.commands.Command;
+import seedu.nova.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses user input for schedule feature.
+ */
+public class ProgresstrackerParser {
+
+    public ProgresstrackerParser() {
+    }
+
+    public Command parseCommand(String userInput) throws ParseException {
+        return null;
+    }
+}
---
+++ src/test/java/seedu/nova/logic/commands/AbListCommandTest.java
@@ -0,0 +1,40 @@
+package seedu.nova.logic.commands;
+
+import static seedu.nova.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.nova.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.nova.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.nova.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.nova.logic.commands.abcommands.AbListCommand;
+import seedu.nova.model.Model;
+import seedu.nova.model.ModelManager;
+import seedu.nova.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for AbListCommand.
+ */
+public class AbListCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new AbListCommand(), model, AbListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        assertCommandSuccess(new AbListCommand(), model, AbListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
