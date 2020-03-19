77efc08c9bfbf55811565bb7820f619a9a83851f
Yap Wen Jun Bryan <52154266+BryanYap972@users.noreply.github.com>
2020-03-19 20:09:27 +0800

    Update ParserUtil.java

--- src/main/java/seedu/nova/logic/parser/ParserUtil.java
+++ src/main/java/seedu/nova/logic/parser/ParserUtil.java
@@ -69,7 +69,8 @@ public class ParserUtil {
         }
         return new Phone(trimmedPhone);
     }
-
+    
+    /**
      * Parses a {@code String email} into an {@code Email}.
      * Leading and trailing whitespaces will be trimmed.
      *
