db29eb4bd3d86a5f041b452a64ab639caae0f22b
wahken_win_ubuntu <t.1999ken@gmail.com>
2020-03-29 22:21:23 +0800

    minor modifications for event classes

--- src/main/java/seedu/nova/model/event/Event.java
+++ src/main/java/seedu/nova/model/event/Event.java
@@ -1,7 +1,12 @@
 package seedu.nova.model.event;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
+
+import seedu.nova.model.util.time.duration.DateTimeDuration;
+import seedu.nova.model.util.time.duration.TimedDuration;
+import seedu.nova.model.util.time.duration.WeekDayDuration;
 
 /**
  * Represents an Event that can be managed.
@@ -14,12 +19,24 @@ public class Event {
     protected LocalDate date;
     protected String note = "";
     protected boolean isDone = false;
+    protected TimedDuration dtd;
 
-    public Event(String desc, String venue, LocalTime startTime, LocalTime endTime) {
+    public Event(String desc, String venue, LocalTime startTime, LocalTime endTime, LocalDate date) {
         this.desc = desc;
         this.venue = venue;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.date = date;
+        this.dtd = new DateTimeDuration(date, startTime, endTime);
+    }
+
+    public Event(String desc, String venue, LocalTime startTime, LocalTime endTime, DayOfWeek dow) {
+        this.desc = desc;
+        this.venue = venue;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.date = null;
+        this.dtd = new WeekDayDuration(dow, startTime, endTime);
     }
 
     public boolean getIsDone() {
@@ -36,6 +53,14 @@ public class Event {
 
     public LocalTime getEndTime() {
         return endTime;
+    }
+
+    public TimedDuration getDtd() {
+        return dtd;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dtd.getStartDay();
     }
 
     public void setDate(LocalDate date) {
