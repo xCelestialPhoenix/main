86045d8646cd9f32ae7f7b86d6ce35a53a228f91
xCelestialPhoenix <terenceng1996@gmail.com>
2020-04-08 21:57:42 +0800

    Written test cases for JsonAdaptedEvents

---
+++ src/test/java/seedu/nova/testutil/TypicalEvents.java
@@ -0,0 +1,27 @@
+package seedu.nova.testutil;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import seedu.nova.model.schedule.event.Consultation;
+import seedu.nova.model.schedule.event.Event;
+import seedu.nova.model.schedule.event.Lesson;
+import seedu.nova.model.schedule.event.Meeting;
+import seedu.nova.model.schedule.event.StudySession;
+
+public class TypicalEvents {
+
+    public static final Event MEETING = new Meeting("Project Meeting", "COM1-B108",
+            LocalTime.of(14, 0), LocalTime.of(15, 0), LocalDate.of(2020, 3, 9));
+
+    public static final Event CONSULTATION = new Consultation("Design Principles", "COM2-0203",
+            LocalTime.of(16, 0), LocalTime.of(17, 0), LocalDate.of(2020, 3, 20));
+
+    public static final Event STUDY_SESSION = new StudySession("UML Diagrams", "Home",
+            LocalTime.of(11, 0), LocalTime.of(12, 0), LocalDate.of(2020, 3, 12));
+
+    public static final Event LESSON = new Lesson("CS2103T Lecture", "I3 AUD",
+            LocalTime.of(14, 0), LocalTime.of(16, 0), DayOfWeek.FRIDAY, LocalDate.of(2020, 3, 27), "Bring charger");
+
+}
