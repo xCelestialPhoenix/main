7b010f9a2b47506e6766888f82ae538eacfa68d1
huixianc <50432304+huixianc@users.noreply.github.com>
2020-04-10 16:19:31 +0800

    added tests for adding lesson

--- src/test/java/seedu/nova/logic/commands/events/EventAddLessonCommandTest.java
+++ src/test/java/seedu/nova/logic/commands/events/EventAddLessonCommandTest.java
@@ -1,15 +1,104 @@
 package seedu.nova.logic.commands.events;
 
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.nova.testutil.Assert.assertThrows;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.nova.logic.commands.CommandResult;
+import seedu.nova.logic.commands.exceptions.CommandException;
+import seedu.nova.logic.commands.sccommands.eventcommands.EventAddConsultationCommand;
 import seedu.nova.logic.commands.sccommands.eventcommands.EventAddLessonCommand;
+import seedu.nova.model.schedule.event.Event;
+import seedu.nova.model.schedule.event.Lesson;
+import seedu.nova.model.schedule.event.TimeOverlapException;
+import seedu.nova.testutil.ModelStub;
 
 public class EventAddLessonCommandTest {
+    private static final Event validLesson = new Lesson("CS2103T Lecture", "I3 AUD",
+            LocalTime.of(14, 0), LocalTime.of(16, 0), DayOfWeek.FRIDAY);
+
     @Test
     public void constructor_nullLesson_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new EventAddLessonCommand(null));
+    }
+
+
+    @Test
+    public void execute_lessonAcceptedByModel_addSuccessful() throws Exception {
+        ModelStubAcceptingLessonAdded modelStub = new ModelStubAcceptingLessonAdded();
+
+        CommandResult commandResult = new EventAddLessonCommand(validLesson).execute(modelStub);
+
+        assertEquals(String.format(EventAddLessonCommand.MESSAGE_SUCCESS, validLesson),
+                commandResult.getFeedbackToUser());
+
+        assertEquals(Arrays.asList(validLesson), modelStub.eventsAdded);
+    }
+
+    @Test
+    public void execute_lessonOverlapsWithOtherEvent_throwsCommandException() {
+
+        ModelStubOverlappingEvent modelStub = new ModelStubOverlappingEvent();
+
+        EventAddLessonCommand eventAddLessonCommand = new EventAddLessonCommand(validLesson);
+
+        assertThrows(CommandException.class, EventAddConsultationCommand.MESSAGE_TIME_OVERLAP, () ->
+                eventAddLessonCommand.execute(modelStub));
+
+    }
+
+
+    @Test
+    public void equals() {
+
+        Event lesson2 = new Lesson("CS2103T Lecture", "I3 AUD",
+                LocalTime.of(14, 0), LocalTime.of(16, 0), DayOfWeek.MONDAY);
+
+        EventAddLessonCommand command1 = new EventAddLessonCommand(validLesson);
+        EventAddLessonCommand command2 = new EventAddLessonCommand(lesson2);
+
+        assertTrue(command1.equals(command1));
+
+        EventAddLessonCommand command1Copy = new EventAddLessonCommand(validLesson);
+
+        assertTrue(command1.equals(command1Copy));
+
+        assertFalse(command1.equals("blah"));
+
+        assertFalse(command1.equals(null));
+
+        assertFalse(command1.equals(command2));
+    }
+
+    private class ModelStubOverlappingEvent extends ModelStub {
+
+        @Override
+        public void addAllLessons(Lesson lesson) {
+            requireNonNull(lesson);
+
+            throw new TimeOverlapException();
+        }
+    }
+
+
+    private class ModelStubAcceptingLessonAdded extends ModelStub {
+        private LinkedList<Event> eventsAdded = new LinkedList<>();
+
+        @Override
+        public void addAllLessons(Lesson lesson) {
+            requireNonNull(lesson);
+
+            eventsAdded.add(lesson);
+        }
     }
 
 
